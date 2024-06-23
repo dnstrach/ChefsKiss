ddc95a7ef0674a76664405d71fd676ed311836e4
dnstrach <dnstrach@gmail.com>
2024-06-22 21:57:00 -0600

    add/edit view

---
+++ ChefsKiss/View/Recipe/EquipmentListView.swift
@@ -0,0 +1,18 @@
+//
+//  EquipmentListView.swift
+//  ChefsKiss
+//
+//  Created by Dominique Strachan on 6/22/24.
+//
+
+import SwiftUI
+
+struct EquipmentListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EquipmentListView()
+}
---
+++ ChefsKiss/View/Recipe/InstructionsListView.swift
@@ -0,0 +1,18 @@
+//
+//  InstructionsListView.swift
+//  ChefsKiss
+//
+//  Created by Dominique Strachan on 6/22/24.
+//
+
+import SwiftUI
+
+struct InstructionsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InstructionsListView()
+}
